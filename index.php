1b76c5bce13bd36365dff08427f89f1a6a04bd86
AsanteZeebra <nanakweku608@yahoo.com>
2025-01-02 09:23:28 +0000

    passs

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
-include_once('load_session.php');
 
-//include_once('connect.php');
+
+include_once('connect.php');
+include_once('load_session.php');
 include_once('check_passports.php');
 
 ?>
